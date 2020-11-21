56ebbd57b4cfeb69221dffd79de0706c4d094b85
gaya3z <gayathrisatheesh@outlook.in>
2020-11-21 19:05:24 +0530

    main

--- src/views/courseDetails.js
+++ src/views/courseDetails.js
@@ -1,14 +1,22 @@
-import { Navbar } from './../components'
+import { BreadCrumb, Navbar } from './../components'
 import React from 'react'
+import { SearchBar } from '@/components/HeroSection/HeroSecElements'
+import { Typography } from '@material-ui/core'
+import { Rating } from './../components/CourseCard/CourseCardElements'
 
 export default function courseDetails() {
     return (
         <>
         <Navbar />
         <Container>
-            <Title>
-                
-            </Title>
+           <BreadCrumb />
+            <SearchBar />
+            <Btn>Search</Btn>
+            <Title>Course Title</Title>
+            <Typography>University Name</Typography>
+            <Rating />
+            <Typography>0 reviews</Typography>
+            
         </Container>
         </>
     )
